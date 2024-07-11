606fb2198ea8203187c54cef45cd23242a3bb26b
Kostylev-Vladislav <kostylev.vladd@yandex.ru>
2024-07-11 09:06:20 +0300

    Update FileCache.swift

--- Sources/FileCache/FileCache.swift
+++ Sources/FileCache/FileCache.swift
@@ -54,3 +54,27 @@ class FileCache {
         }
     }
 }
+struct ToDoItem {
+    let id: String
+    var text: String
+    var importance: Importance
+    var isDone: Bool
+    let createdDate: Date
+    var deadline: Date?
+    var changedDate: Date?
+    var colorHex: String?
+    var category: TaskCategory
+    
+    init(id: String = UUID().uuidString, text: String, importance: Importance = .normal,isDone: Bool = false, createdDate: Date = Date(), deadline: Date? = nil, changedDate: Date? = nil, colorHex: String? = nil, category: TaskCategory = .other) {
+        self.id = id
+        self.text = text
+        self.importance = importance
+        self.isDone = isDone
+        self.createdDate = createdDate
+        self.deadline = deadline
+        self.changedDate = changedDate
+        self.colorHex = colorHex
+        self.category = category
+    }
+}
+
