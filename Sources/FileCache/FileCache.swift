49f540fd3fe12f3b6167e1ca160c3fdf83cf5453
Kostylev-Vladislav <kostylev.vladd@yandex.ru>
2024-07-11 09:08:36 +0300

    Update FileCache.swift

--- Sources/FileCache/FileCache.swift
+++ Sources/FileCache/FileCache.swift
@@ -29,30 +29,30 @@ class FileCache {
         tasks.removeAll {$0.id == id}
     }
     
-    func save(filename: String) throws {
-        let jsonArray = tasks.map { $0.json }
-        do {
-            let jsonData = try JSONSerialization.data(withJSONObject: jsonArray, options: .prettyPrinted)
-            let fileURL = URL(filePath: filename)
-            try jsonData.write(to: fileURL)
-        } catch {
-            throw FileCacheError.saveError
-        }
-    }
+    // func save(filename: String) throws {
+    //     let jsonArray = tasks.map { $0.json }
+    //     do {
+    //         let jsonData = try JSONSerialization.data(withJSONObject: jsonArray, options: .prettyPrinted)
+    //         let fileURL = URL(filePath: filename)
+    //         try jsonData.write(to: fileURL)
+    //     } catch {
+    //         throw FileCacheError.saveError
+    //     }
+    // }
         
-    func load(filename: String) throws {
-        let fileURL = URL(filePath: filename)
-        do {
-            let jsonData = try Data(contentsOf: fileURL)
-            if let jsonArray = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [Any] {
-                tasks = jsonArray.compactMap { ToDoItem.parse(json: $0) }
-            } else {
-                throw FileCacheError.invalidData
-            }
-        } catch {
-            throw FileCacheError.loadError
-        }
-    }
+    // func load(filename: String) throws {
+    //     let fileURL = URL(filePath: filename)
+    //     do {
+    //         let jsonData = try Data(contentsOf: fileURL)
+    //         if let jsonArray = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [Any] {
+    //             tasks = jsonArray.compactMap { ToDoItem.parse(json: $0) }
+    //         } else {
+    //             throw FileCacheError.invalidData
+    //         }
+    //     } catch {
+    //         throw FileCacheError.loadError
+    //     }
+    // }
 }
 struct ToDoItem {
     let id: String
